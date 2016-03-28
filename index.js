07e1a17e2c066d54b53d899b9cf0a2f7b661b96e
Linus Unnebäck <linus@folkdatorn.se>
2016-03-28 02:44:53 +0200

    remove garbage

--- index.js
+++ index.js
@@ -3,7 +3,3 @@
 exports.generateSolvedPuzzle = require('./lib/generate-solved-puzzle')
 exports.validatePuzzle = require('./lib/validate-puzzle')
 exports.printPuzzle = require('./lib/print-puzzle')
-
-const p = exports.generateSolvedPuzzle()
-console.log(exports.validatePuzzle(p))
-exports.printPuzzle(p)
